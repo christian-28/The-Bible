7203fe68b5692c4bd66938dcda0ad3b4c4da6c07
Christian <newbiedev01@gmail.com>
2023-07-04 23:46:17 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+   document.getElementById("myDiv").classList.toggle("show");
+}
+
+function myFunctionSecond() {
+   document.getElementById("myDivSecond").classList.toggle("show");
+}
